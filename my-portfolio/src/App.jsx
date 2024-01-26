c4c09d54d6f62351e9469fded7773ff1f2404b5c
wande <yewandeadeyemi84@gmail.com>
2024-01-26 17:20:26 +0100

    portofolio

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Home } from './components'
+import { About } from './components'
+import {Contact } from './components'
+import { Projects } from './components'
+
+
+function App() {
+  return (
+    <>
+     <Home />
+     <About />
+     <Contact />
+     <Projects />
+
+    </>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/components/index.js
@@ -0,0 +1,7 @@
+import About from "./about";
+import Contact from "./contact";
+import Home from "./home";
+import Projects from "./projects";
+
+
+ export {About,Contact,Home,Projects}
